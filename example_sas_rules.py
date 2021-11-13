60ced5bf059c79e9515d516742c48e89de10c934
Jules Chassetuillier <jules-chstlr@MacBook-Pro-de-Jules-Chassetuillier.local>
2021-11-13 11:29:43 +0100

    Example with iris dataset and RandomForrestClassifier

---
+++ example_sas_rules.py
@@ -0,0 +1,27 @@
+""" Script for extracting decision trees rules into SAS code"""
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from tree_to_sas import *
+
+
+# Example with iris dataset
+iris = datasets.load_iris()
+X = pd.DataFrame(iris.data, columns=iris.feature_names)
+y = iris.target
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=101)
+rf = RandomForestClassifier(n_estimators=100)
+rf.fit(X_train, y_train)
+
+# Get SAS rules for all trees
+sas_tree_rules = {}
+for i, tree in enumerate(rf):
+    tree_name = "TREE_{}".format(i)
+    tree_rules = get_rules(tree=rf[i], tree_id=i, features=iris.feature_names, sas_table="DATASET")
+    sas_tree_rules[tree_name] = tree_rules
+
+# Output of first Tree rules in SAS
+print(sas_tree_rules["TREE_0"])
+
